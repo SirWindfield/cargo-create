b87e216df067429f6a4045d53102ec33ffc24913
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-08-24 02:04:10 +0200

    feat: move/rename files
    The `files` field inside of the `jen.toml` file now takes two types of
    values inside the array. A simple string that points to a file.
    Additionally, an inline table with the values `from` and `to` can be
    used to move files inside the template repository during the feature
    resolve phase.
    
    Moving can be used to structure template repositories and make their
    development a little bit more bearable. Renaming files can be used if
    different features want to a file with the same name.
    
    Closes #32

--- src/steps/apply.rs
+++ src/steps/apply.rs
@@ -1,8 +1,13 @@
-//! Step 2: Apply features and delete all unneeded files.
+//! Step 2: Apply features and delete all unneeded files as well as rename those
+//! that are kept.
 
 use crate::{
     args::Args,
-    config::{template::TemplateConfig, user::UserConfig, CONFIG_FILENAME_JEN},
+    config::{
+        template::{FileEntry, MoveFileEntry, TemplateConfig},
+        user::UserConfig,
+        CONFIG_FILENAME_JEN,
+    },
 };
 use anyhow::Result;
 use std::{
@@ -10,6 +15,8 @@ use std::{
     io::Read,
     path::Path,
 };
+
+pub type FilesToRemoveAndRename<'a> = (Vec<&'a str>, Vec<MoveFileEntry<'a>>);
 
 pub fn run(
     args: &Args,
@@ -25,9 +32,12 @@ pub fn run(
         let template_config: TemplateConfig = toml::from_str(&content)?;
 
         // Only check if the user actually specified some features that he wants.
-        let files_to_delete: Vec<&str> = if let Some(features) = &args.features {
+        let files_to_operate_on: FilesToRemoveAndRename = if let Some(features) = &args.features {
             let slice_of_features = &template_config.features.iter().collect::<Vec<_>>();
 
+            // Holds all features that have been enabled by the user. The feature have
+            // already been expanded, meaning that super-features are resolved into their
+            // smaller parts.
             let mut resolved_features = Vec::new();
             features
                 .iter()
@@ -38,32 +48,78 @@ pub fn run(
                     resolved_features.extend_from_slice(&feat);
                 });
 
-            template_config
-                .features
-                .iter()
-                .filter(|feat| {
+            // Partition the files based on whether they are included or not. Included files
+            // need to be renamed. Excluded files deleted. The left vector contains all
+            // excluded files, the right one all included ones.
+            let files_to_operate_on: (Vec<_>, Vec<_>) =
+                template_config.features.iter().partition(|feat| {
                     !resolved_features
                         .iter()
                         .any(|feat_name| feat.name == feat_name.name)
-                })
+                });
+            println!("partitioned: {:#?}", files_to_operate_on);
+
+            // Collect all files to rename.
+            let files_to_rename = files_to_operate_on
+                .1
+                .iter()
                 .map(|feat| feat.files.as_ref())
                 .flatten()
                 .flatten()
                 .copied()
-                .collect::<Vec<_>>()
-        } else {
-            template_config
-                .features
+                .map(|entry| match entry {
+                    FileEntry::Complex(inner) => Some(inner),
+                    _ => None,
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+
+            let files_to_delete = files_to_operate_on
+                .0
                 .iter()
-                .map(|feat| &feat.files)
+                .map(|feat| feat.files.as_ref())
                 .flatten()
                 .flatten()
                 .copied()
-                .collect::<Vec<_>>()
+                .map(|entry| match entry {
+                    FileEntry::Simple(filename) => filename,
+                    FileEntry::Complex(MoveFileEntry {from, ..}) => from,
+                })
+                .collect::<Vec<_>>();
+
+            (files_to_delete, files_to_rename)
+        } else {
+            (
+                template_config
+                    .features
+                    .iter()
+                    .map(|feat| &feat.files)
+                    .flatten()
+                    .flatten()
+                    .copied()
+                    .map(|entry| match entry {
+                        FileEntry::Simple(ref filename) => filename,
+                        FileEntry::Complex(MoveFileEntry { from, .. }) => from,
+                    })
+                    .collect::<Vec<_>>(),
+                Vec::new(),
+            )
         };
 
-        for file in files_to_delete {
+        println!("{:#?}", &files_to_operate_on);
+
+        // Delete all files that are not needed.
+        for file in &files_to_operate_on.0 {
             fs::remove_file(template_repo_dir.as_ref().join(file))?;
+        }
+
+        // Rename files.
+        for file in &files_to_operate_on.1 {
+            let dir = template_repo_dir.as_ref();
+            let from = &dir.join(file.from);
+            let to = &dir.join(file.to);
+
+            fs::rename(from, to)?;
         }
     }
 
